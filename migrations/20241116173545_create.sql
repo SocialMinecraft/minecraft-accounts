8715f17e6d24834802e85c08335d3a5bf4d5f64f
Dylan Garvis <dylan@garvis.dev>
2024-11-16 12:40:03 -0500

    chore: Setup service.

---
+++ migrations/20241116173545_create.sql
@@ -0,0 +1,8 @@
+create table accounts (
+    id bigserial primary key,
+    user_id VARCHAR(100) NOT NULL,
+    first_name VARCHAR(100) NOT NULl,
+    minecraft_uuid VARCHAR(250) unique not null,
+    minecraft_username VARCHAR(50) unique not null,
+    is_main bool not null
+);
