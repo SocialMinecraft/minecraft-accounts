f5d6317c2144e5d4bbcbf64d4dabc9e1664feb65
Dylan Garvis <dylan@garvis.dev>
2025-01-25 00:09:57 -0500

    bug: minecraft name assigned as first name.

--- src/handlers/add.rs
+++ src/handlers/add.rs
@@ -50,7 +50,7 @@ pub async fn add(db: Store, nc: Client, msg: async_nats::Message) -> anyhow::Res
         let mut account = MinecraftAccount::new();
         account.minecraft_username = request.minecraft_username.clone();
         account.minecraft_uuid = request.minecraft_uuid.clone().unwrap();
-        account.deprecated_first_name = request.minecraft_username.clone();
+        account.deprecated_first_name = request.first_name.clone();
 
         // Is their first account (and thus main account)
         {
